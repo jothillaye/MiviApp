86cb41ab21c98c4dff7d10c82fe4642061708d35
Joachim Thillaye <jo.thillaye@gmail.com>
2013-02-01 14:24:01 +0100

    Membre managing: - view : add list and fields to edit members - dataAccess : newMember, getMember(s)

--- src/modelPackage/Membre.java
+++ src/modelPackage/Membre.java
@@ -1,32 +1,40 @@
 package modelPackage;
 
+import java.text.DateFormat;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
-public class Client {
-	private int idResp;
-	private String nom, prenom, nomPreResp;
+public class Membre {
+    private Integer id;
+	private String nom, prenom;
 	private GregorianCalendar dateNaiss;
 
-	public Client(int idResp, String nom, String prenom, GregorianCalendar dateNaiss) {
-		this.idResp = idResp;
+	public Membre(String nom, String prenom, GregorianCalendar dateNaiss) {
 		this.nom = nom;
 		this.prenom = prenom;
 		this.dateNaiss = dateNaiss;		
 	}
 	
 	// Constructeur pour lister les clients lors d'une requ�te sql
-	public Client() {}
+	public Membre() {}
 
 	// Getters
-	public int getIdResp() { return idResp; }
+    public Integer getId() { return id;}
 	public String getNom() { return nom;}
 	public String getPrenom() { return prenom;}
 	public GregorianCalendar getDateNaiss() { return dateNaiss;}
-	public String getNomPreResp() { return nomPreResp;}	
+    
+    public String getFormatedDateNaiss() {    
+        TimeZone tz = TimeZone.getTimeZone("Europe/Brussels");
+        DateFormat df = DateFormat.getDateTimeInstance();  
+        df.setTimeZone(tz);          
+        return df.format(dateNaiss.getTime());
+    }
 
 	// Setters
+    public void setId(Integer id) { this.id = id;}
 	public void setNom(String nom) { this.nom = nom;}
 	public void setPrenom(String prenom) { this.prenom = prenom;}
 	public void setDateNaiss(GregorianCalendar dateNaiss) { this.dateNaiss = dateNaiss;}
-	public void setNomPreResp(String nomPreResp) { this.nomPreResp = nomPreResp;}
+    
 }
